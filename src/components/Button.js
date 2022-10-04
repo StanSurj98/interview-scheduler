c1ff750701e56f199596928a3734d9238be22f7d
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-04 16:35:57 -0600

    use storybook to test <Button /> in isolated cases for danger, confirm, base, clickable, and disabled

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,18 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+   // Conditional styling if these props were passed down, add the class modifier to it
+   if (props.confirm) buttonClass += " button--confirm";
+   if (props.danger) buttonClass += " button--danger";
+
+
+   return (
+      <button className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   ); 
+};
