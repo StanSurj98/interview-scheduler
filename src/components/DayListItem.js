6051aad36b52ccef78ba77cad5090fb687ca709f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-04 17:56:48 -0600

    add conditional styling to the DayListItem with classnames library

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "components/DayListItem.scss";
 import classNames from "classnames";
 
 
 export default function DayListItem(props) {
+  // Using classNames lib for conditional class application
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected, // if truthy, add this modifier class
+    "day-list__item--full": props.spots === 0 // if truthy, add this modifier class
+  });
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+    onClick={() => props.setDay(props.name)}
+    className={dayClass}
+    >
       <h2 
       className="text--regular" 
       >
