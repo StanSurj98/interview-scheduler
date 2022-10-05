2fded74c1a239571274ec72ea6a1a0188f078036
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-04 18:07:37 -0600

    refactor 'spots remaining' text output and pass all jest tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,9 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 
+
+
+
 export default function DayListItem(props) {
   // Using classNames lib for conditional class application
   const dayClass = classNames({
@@ -10,6 +13,13 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected, // if truthy, add this modifier class
     "day-list__item--full": props.spots === 0 // if truthy, add this modifier class
   });
+
+  // Helper func => allows for unique text at 0 or 1 spot(s) remaining
+  const formatSpots = (spots) => {
+    if (spots === 0) return "no spots remaining";
+    if (spots === 1) return "1 spot remaining";
+    return `${spots} spots remaining`
+  }
 
   return (
     <li 
@@ -21,7 +31,7 @@ export default function DayListItem(props) {
       >
         {props.name}
       </h2>
-      <h3 className="text--light" >{props.spots} spots remaining</h3>
+      <h3 className="text--light" >{formatSpots(props.spots)}</h3>
     </li>
   );
 }
