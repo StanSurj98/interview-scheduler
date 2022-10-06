493dfb731c6f2d24511800a62277e1fc87b61891
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-05 18:06:06 -0600

    add <Confirm /> and its stories>

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 // <Button />
 storiesOf("Button", module)
@@ -178,5 +179,12 @@ storiesOf("Appoinment", module)
         onDelete={action("onDelete")}
         student="Lydia Miller-Jones"
         interviewer={interviewer} // from above
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm 
+        message="Delete the appointment?"
+        onCancel={action("onCancel")}
+        onConfirm={action("onConfirm")}
       />
     ))
