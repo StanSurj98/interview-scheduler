fd434d3c818267319153a42daecf520b4de4b562
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-05 19:01:04 -0600

    fill in code for the <Form />

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  // We need a blank default if no student props passed down, AKA we're entering our name into the form for the first time
+  // HOWEVER - if we're EDITING our form, we want the existing value to be props.student
+  const [student, setStudent] = useState(props.student || "");
+  // My DEFAULT is props.{student/interviewer} if truthy or ""/null 
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,14 +19,19 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            // Value of the form input is the student state
+            value={student}
+            // this onChange allow us to see irl the effects of typing, re-renders each char
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          // pass in the interviewers array so it can render each list item
+          interviewers={props.interviewers}
+          // pass in props of value to be the interviewer state in this form
+          value={interviewer}
+          // pass in props of onChange which is NOT keyword, but the setInterviewer useState
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
