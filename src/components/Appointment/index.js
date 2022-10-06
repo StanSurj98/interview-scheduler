530a45fbaf1c0c7c3e282fde8851bc65729c7759
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-05 20:07:20 -0600

    empty and show stories and conditionally render them in appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 // We wrote this component inside the Appointment/ directory because it will have lots of child components later, and index.js is used to import easily as a default path
 export default function Appointment(props) {
   
   return (
-    <article className="appointment"></article>
+    <Fragment>
+      <Header time={props.time}/>
+      {/* Conditional render if props.interview truthy */}
+      {props.interview ? 
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer } 
+        /> : 
+        <Empty />}
+
+    </Fragment>
   );
 };
