31bb00c01b04577b6ca4959854222500cdf92949
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-05 18:09:29 -0600

    add <Status /> and its story

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // <Button />
 storiesOf("Button", module)
@@ -186,5 +187,10 @@ storiesOf("Appoinment", module)
         message="Delete the appointment?"
         onCancel={action("onCancel")}
         onConfirm={action("onConfirm")}
+      />
+    ))
+    .add("Status", () => (
+      <Status 
+        message="Deleting"
       />
     ))
