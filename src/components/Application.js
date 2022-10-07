52ea97a8f97b2facfae08ab663650ad4417523c9
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 10:54:29 -0600

    combined states into object, created 2 setDay/setDays function that updates state

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,49 +10,66 @@ import Appointment from "components/Appointment";
 
 
 // Temp interview data
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([]);
+  // Managing multiple states inside an object
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}, // this is going to break temporarily since we have our fake data above
+  });
+
+  const dailyAppointments = [];
+
+  // Creating new setDay function with the object above
+  const setDay = day => setState({ ...state, day });
+  // What this does, uses spread operator on state object, says:
+  // "Give me all the properties of state, but update the one that matches day argument in this func"
+
+  // Same with setDays
+  const setDays = (days) => { 
+    setState(prev => ({...prev, days}));
+  };
+  // Take the array of objects in "state" obj, which is under "days":[] and replace it with the func argument
 
 
   // Axios GET to /api/days ONLY once on initial render (dep: [])
@@ -60,6 +77,7 @@ export default function Application(props) {
     axios.get(`http://localhost:8001/api/days`)
       .then(response => {
         console.log(response);
+        // setDays affected from combining states into object
         setDays(response.data)
       })
       .catch(error => {console.log("There is an error: \n", error)});
@@ -88,12 +106,12 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            // Changed the name of this prop to make more sense WHY we passed it down
-            value={day}
-            // since we're passing to <ul> component, we need value and onChange anyways
+            // Now that we're managing states in an object, we make sure to update it
+            days={state.days}
+            value={state.day}
+            // Even if setDay is called on a grandchild x10 component, it'll come all the way up here, look for that state object at the top of Application component
+            // And executes the changes on this top level!
             onChange={setDay}
-            // !! REMEMBER !! -> these aren't the KEYWORDS "value" and "onChange" event, these are just our props name following the HTML convention!!
           />
         </nav>
         <img
