d5b65111e8cc8a0bcaa33a3d3cf631370fc15d01
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 00:27:11 -0600

    solve the selector function for jest

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -32,12 +32,43 @@ const state = {
 
 
 export const getAppointmentsForDay = (stateObj, dayArg) => {
+  // If days array is empty, return empty array
+  if (stateObj.days.length === 0) return [];
+
+  const appointmentIDs = [];
+
   // Need to find the object in state.days who's name matches the day argument
   for (const day of stateObj.days) {
+    if (day.name !== dayArg) continue;
     if (day.name === dayArg) {
-      return day.appointments;
+      // When the day name matches the given day, push into our empty array the array of appointment IDs for that day
+      appointmentIDs.push(...day.appointments);
     }
   }
+
+
+  const results = appointmentIDs.map((id) => {
+    return stateObj.appointments[id];
+  });
+
+  return results;
+
+  // Using .find() worked out with travis
+  // let daysArray = state.days
+  // let appointmentArray = [];
+  // // if (daysArray.length === 0) return appointmentArray;
+
+  // let dayTarget = daysArray.find((dayObject)=> {
+  //   return dayObject.name === day
+  // })
+  // if (!dayTarget) return appointmentArray;
+  // let daysAppointments = [...dayTarget.appointments]
+
+  // appointmentArray = daysAppointments.map((appointmentID) => {
+  //     return state.appointments[appointmentID];
+  //   })
+
+  //   return appointmentArray;
 };
 
-// console.log(getAppointmentsForDay(state, "Monday"));
+// console.log(`this is 55: `, getAppointmentsForDay(state, "Monday"));
