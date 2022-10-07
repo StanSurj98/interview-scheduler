da001ea453a6fbfb8f5c9ce170233964fa4d4e75
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 09:47:11 -0600

    clean up selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,36 +1,3 @@
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  }
-};
-
-
-
-
 export const getAppointmentsForDay = (stateObj, dayArg) => {
   // If days array is empty, return empty array
   if (stateObj.days.length === 0) return [];
@@ -70,5 +37,3 @@ export const getAppointmentsForDay = (stateObj, dayArg) => {
 
   //   return appointmentArray;
 };
-
-// console.log(`this is 55: `, getAppointmentsForDay(state, "Monday"));
