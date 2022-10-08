6f42430d9b4a85a1166fe72842a77fa6896eec6f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 20:49:25 -0600

    wrote back functionality and pass tests up until replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,20 +1,31 @@
 import { useState, useEffect } from 'react';
 
 const useVisualMode = (initial) => {
-  // Takes an initial mode, sets the state to it, set mode to an array so that we can access previous mode?
+  // Takes an initial mode, sets the state to it
   const [mode, setMode] = useState(initial);
+  // We need a state that is an array to keep track of all modes we've been through. call it history
+  const [history, setHistory] = useState([initial]);
 
-  const modeArr = []
 
-  // Creating a transition function that will take us to the next mode declared and change our state
+  // Adds nextMode to history state array, important that we useState for this too, so it persists
   const transition = (nextMode) => {
-    modeArr.push(mode);
-    setMode(nextMode);
+    // grabbing whole copy of history array, add on the current mode
+    setHistory(prev => [...prev, mode]);
+    // then setMode from previous state, to next mode
+    setMode(prev => nextMode);
   };
 
+  const back = () => {
+    // Limit, don't allow it to go back past the initial mode in our history
+    if (history.length === 1) return [initial];
+    // using .pop() returns the last element REMOVING it from the history array - we mutate it
+    setMode(prev => history.pop())
+    // then making sure we update history state from prev to this (do we need to? since it's mutating?)
+    setHistory(prev => history)
+  }
 
   // Ultimately, we want to return an object with that mode as its property
-  return { mode: mode, transition }; // return as key:val so that tests/components can access it later
-}
+  return { mode: mode, transition, back }; // return as key:val so that tests/components can access it later
+};
 
 export default useVisualMode;
