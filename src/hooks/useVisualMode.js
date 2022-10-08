23d7057e6048f6ae238b59a7016f33e6614f1320
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 18:44:57 -0600

    wrote first initial mode test for useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,12 @@
-const useVisualMode = () => {
+import { useState } from 'react';
 
+const useVisualMode = (initial) => {
+  // Takes an initial mode, sets the state to it
+  const [mode, setMode] = useState(initial);
+
+
+  // Ultimately, we want to return an object with that mode as its property
+  return { mode: mode }; // return as key:val so that tests/components can access it later
 }
 
 export default useVisualMode;
