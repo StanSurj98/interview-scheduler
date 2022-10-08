7e9658a12f1c9316a57edaaa168a94b6f1e6a0d5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-07 19:15:37 -0600

    add test and wrote function for second mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useVisualMode = (initial) => {
-  // Takes an initial mode, sets the state to it
+  // Takes an initial mode, sets the state to it, set mode to an array so that we can access previous mode?
   const [mode, setMode] = useState(initial);
+
+  const modeArr = []
+
+  // Creating a transition function that will take us to the next mode declared and change our state
+  const transition = (nextMode) => {
+    modeArr.push(mode);
+    setMode(nextMode);
+  };
 
 
   // Ultimately, we want to return an object with that mode as its property
-  return { mode: mode }; // return as key:val so that tests/components can access it later
+  return { mode: mode, transition }; // return as key:val so that tests/components can access it later
 }
 
 export default useVisualMode;
