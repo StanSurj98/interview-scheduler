877908c7a64c04d6fe2041f661a5ddb76bc2e5c8
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-10 22:51:51 -0600

    deletes the interview from the API server and the client

--- src/components/Application.js
+++ src/components/Application.js
@@ -26,7 +26,7 @@ export default function Application(props) {
     interviewers: {},
   });
 
-  console.log("STATES AT APP.JS\n\n\n", state);
+  // console.log("STATES AT APP.JS\n\n\n", state);
 
   //
   // ----- Axios & Side Effects-----
@@ -67,9 +67,9 @@ export default function Application(props) {
   // interviewers for day to select from in <Form />
   const dailyInterviewers = getInterviewersForDay(state, state.day);
 
-  // 
+  //
   // ----- State Update w/ PUT to API server -----
-  // 
+  //
 
   // bookInterview - UPDATING appointment info with new interview object from <Form /> onSave()
   const bookInterview = (id, interview) => {
@@ -86,15 +86,54 @@ export default function Application(props) {
       [id]: appointment,
     };
 
-
     // Ultimately want to setState() here to update the overall STATE object but AFTER a PUT req
     // !! REMEMBER !! must wrap the data body in the form you need, in this case an object with key "interview" so we can just encapsulate it with {...} like this
-    axios.put(`/api/appointments/${id}`, { interview })
+    return axios
+      .put(`/api/appointments/${id}`, { interview })
       .then(() => {
-        setState({...state, appointments})
+        setState({ ...state, appointments });
       })
-      .catch(e => console.log("error at line100 application.js", e));
+      .catch((e) => console.log("error at line100 application.js", e));
+  };
 
+  //
+  // ----- Deleting Interviews -----
+  //
+  const cancelInterview = (id, interview) => {
+    // Psuedo:
+    // Need to check the interview object at the appointment id, passed up from <Appointment />
+    // Eventually, set it {interview: null} but presumably after the axios.DELETE goes through
+
+    // have to update the state object nullifying this particular interview object, step by step
+    const appointment = {
+      // copy of all details for THIS appointment ID
+      ...state.appointments[id],
+      // BUT, update interview key to be null
+      interview: null,
+    };
+
+    // one level higher up the state object, want to update state.appointments
+    const appointments = {
+      // keep all details the same for ALL the appointments...
+      ...state.appointments,
+      // EXCEPT, the appointment at THIS id, apply the changes above to interview
+      [id]: appointment,
+    };
+
+    // I have the current id for the path, and I now have the interview object being passed up
+    return axios
+      .delete(`/api/appointments/${id}`, { interview })
+      .then(() => {
+        console.log("Axios.delete is successful for this interview");
+        // when axios delete is successful, update State with setState, keep all details BUT update the appointments property with the changes above
+        setState({ ...state, appointments });
+        console.log(state.appointments);
+      })
+      .catch((e) =>
+        console.log(
+          "Something went wrong with axios, line 133, cancelInterview()"
+        )
+      );
   };
 
   // Mapping each <Appointment /> from dailyAppointments array of appointment objects returned by our selector helper
@@ -109,6 +148,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={dailyInterviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 // Constants for Modes that we will need
@@ -12,9 +13,14 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const SAVING = "SAVING";
 const CREATE = "CREATE";
+const CONFIRM = "CONFIRM";
+const DELETING = "DELETING";
 
 // We wrote this component inside the Appointment/ directory because it will have lots of child components later, and index.js is used to import easily as a default path
 export default function Appointment(props) {
+  // console.log("Props for Appointment: \n", props);
+
+
   // Using our useVisualMode custom hook - remember, mode is a state, on change we render
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY // determines the initial mode based on if props.interview truthy
@@ -40,30 +46,76 @@ export default function Appointment(props) {
       .then(() => {
         transition(SHOW);
       })
+      .catch((e) => console.log("Error at line 49 in bookInterview: ", e))
   }
 
+  const deleteInterview = () => {
+    // creates the object to pass UP to our cancelInterview() in app.js with the name and interviewer for THIS appointment id
+    const interview = {
+      // which was made available as props to us in app.js when we rendered each appointment card
+      student: props.interview.student,
+      interviewer: props.interview.interviewer,
+    }
+    
+    // For <Status /> Loading when confirming the deletion
+    transition(DELETING);
+
+    // Call on cancelInterview() UP in app.js parent, do an axios.delete and come back to us
+    props.cancelInterview(props.id, interview)
+      .then(() => {
+        // if all goes well, return the mode === EMPTY
+        transition(EMPTY);
+      })
+      .catch((e) => console.log("Error at line 68 deleteInterview: ", e))
+  }
 
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
+
       {/* Conditional render on modes with useVisualMode */}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} /> }
+
+
       {/* SAVING */}
       {mode === SAVING && <Status message={"Booking interview..."} />}
+      
+      
       {/* SHOW */} 
       {mode === SHOW && (
         <Show 
           student={props.interview.student} 
           interviewer={props.interview.interviewer} 
+          onDelete={() => transition(CONFIRM)}
         /> 
       )}
+
+
       {/* CREATE */}
       {mode === CREATE && (
         <Form 
           interviewers={props.interviewers} 
           onSave={save}
           onCancel={back}  
+        />
+      )}
+
+
+      {/* CONFIRM DELETION */}
+      {mode === CONFIRM && (
+        <Confirm 
+          message={"Do you want to delete your appointment?"} 
+          onCancel={back} 
+          onConfirm={deleteInterview} 
+        />
+      )}
+
+
+      {/* DELETING Status */}
+      {mode === DELETING && (
+        <Status 
+          message={"Deleting interview..."} 
         />
       )}
 
