2fdd385e2bf7f380b01d2866f8b63a7808657b1e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-11 15:31:29 -0600

    refactor, move state logic to useApplicationData.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,135 +1,34 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 // Styles
 import "components/Application.scss";
 // Components
 import DayList from "./DayList";
-import "components/Appointment";
 import Appointment from "components/Appointment";
+// import "components/Appointment";
 // Helpers
 import {
   getAppointmentsForDay,
   getInterview,
   getInterviewersForDay,
 } from "../helpers/selectors";
+import useApplicationData from "../hooks/useApplicationData"
 
 export default function Application(props) {
   //
-  // ----- State and other Variables -----
+  // ----- States and State Functions -----
   //
+  const {state, setDay, bookInterview, cancelInterview} = useApplicationData();
 
-  // Managing multiple states inside an object
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}, // this is going to break temporarily since we have our fake data above
-    interviewers: {},
-  });
 
-  // console.log("STATES AT APP.JS\n\n\n", state);
-
-  //
-  // ----- Axios & Side Effects-----
-  //
-
-  // Axios GET to /api/days ONLY once on initial render (dep: [])
-  useEffect(() => {
-    // Using Promise.all([array of async axios calls]).then(resolve ALL).catch(ANY error)
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
-    ])
-      .then((response) => {
-        // Making variables here that match the state variables up top
-        const days = response[0].data;
-        const appointments = response[1].data;
-        const interviewers = response[2].data;
-        // Setting state simultaneously, when ALL the promises resolved, to new days/apts
-        setState((prev) => ({ ...prev, days, appointments, interviewers }));
-      })
-      .catch((error) => {
-        console.log("There is an error: \n", error);
-      });
-  }, []);
-
-  //
-  // ----- setState Functions -----
-  //
-
-  // Update the state.day property to the day argument passed in this fn
-  const setDay = (day) => setState({ ...state, day });
-
-  // takes the current state object (which is updated with our axios requests) and for the current state.day (which is selected in DayList), format the appointments objects
+  // Find all appointments for the current "day" state
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  // console.log(dailyAppointments);
 
-  // interviewers for day to select from in <Form />
+  // All interviewers for day, to select pictures during <Form /> entry
   const dailyInterviewers = getInterviewersForDay(state, state.day);
 
-  //
-  // ----- State Update w/ PUT to API server -----
-  //
+  
 
-  // bookInterview - UPDATING appointment info with new interview object from <Form /> onSave()
-  const bookInterview = (id, interview) => {
-    // Start updating the STATE object from the lowest level upwards (since it's a nested object)
-    const appointment = {
-      ...state.appointments[id],
-      // Lowest level is the new interview object from <Form /> onSave()
-      interview: { ...interview },
-    };
-
-    // Moving up a level from appointment is the appointment(s) key for the state object
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    // Ultimately want to setState() here to update the overall STATE object but AFTER a PUT req
-    // !! REMEMBER !! must wrap the data body in the form you need, in this case an object with key "interview" so we can just encapsulate it with {...} like this
-    return axios
-      .put(`/api/appointments/${id}`, { interview })
-      .then(() => {
-        setState({ ...state, appointments });
-      })
-  };
-
-  //
-  // ----- Deleting Interviews -----
-  //
-  const cancelInterview = (id, interview) => {
-    // Psuedo:
-    // Need to check the interview object at the appointment id, passed up from <Appointment />
-    // Eventually, set it {interview: null} but presumably after the axios.DELETE goes through
-
-    // have to update the state object nullifying this particular interview object, step by step
-    const appointment = {
-      // copy of all details for THIS appointment ID
-      ...state.appointments[id],
-      // BUT, update interview key to be null
-      interview: null,
-    };
-
-    // one level higher up the state object, want to update state.appointments
-    const appointments = {
-      // keep all details the same for ALL the appointments...
-      ...state.appointments,
-      // EXCEPT, the appointment at THIS id, apply the changes above to interview
-      [id]: appointment,
-    };
-
-    // I have the current id for the path, and I now have the interview object being passed up
-    return axios
-      .delete(`/api/appointments/${id}`, { interview })
-      .then(() => {
-        console.log("Axios.delete is successful for this interview");
-        // when axios delete is successful, update State with setState, keep all details BUT update the appointments property with the changes above
-        setState({ ...state, appointments });
-      })
-  };
-
-  // Mapping each <Appointment /> from dailyAppointments array of appointment objects returned by our selector helper
+  // Mapping each <Appointment /> from dailyAppointments array of apt. objs.
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
@@ -144,7 +43,9 @@ export default function Application(props) {
         cancelInterview={cancelInterview}
       />
     );
+
   });
+
 
   //
   // ----- JSX Template -----
@@ -161,11 +62,10 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            // Now that we're managing states in an object, we make sure to update it
+            // Making sure we pass down the state object data
             days={state.days}
             value={state.day}
-            // Even if setDay is called on a grandchild x10 component, it'll come all the way up here, look for that state object at the top of Application component
-            // And executes the changes on this top level!
+            // setDay for selecting <DayListItem /> and change day state up here
             onChange={setDay}
           />
         </nav>
@@ -176,7 +76,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
+        {/* Populate the daily schedules with each <Appointment /> */}
         {schedule}
+        {/* Hard code the last appointment slot that is always unavailable */}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
