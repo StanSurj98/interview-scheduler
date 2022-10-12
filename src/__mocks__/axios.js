a68b80fe6aa74154e123dbeb69ec5779c53b9993
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 10:02:18 -0600

    add new mock module for axios jest testing

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,96 @@
+/* 
+  
+This is a mock Module file, it will simulate all of our axios during jest
+
+Below are Fixtures - Static Files that are FROM the real API but set in stone here
+Jest will automatically replace any axios import with this mock module instead
+
+*/
+
+const fixtures = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2],
+      interviewers: [1, 2],
+      spots: 1,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [3, 4],
+      interviewers: [3, 4],
+      spots: 1,
+    },
+  ],
+  appointments: {
+    1: { id: 1, time: "12pm", interview: null },
+    2: {
+      id: 2,
+      time: "1pm",
+      interview: { student: "Archie Cohen", interviewer: 2 },
+    },
+    3: {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Leopold Silvers", interviewer: 4 },
+    },
+    4: { id: 4, time: "3pm", interview: null },
+  },
+  interviewers: {
+    1: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+    2: {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png",
+    },
+    3: {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png",
+    },
+    4: {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg",
+    },
+  },
+};
+
+// Here we're just returning an object that would emulate exactly how Axios works
+// Notice, we conditionally check the URL for the request, and give back the hardcoded version of what we usually would get back from the request using Axios
+// Note the resolved promises as a return as well!!
+export default {
+  get: jest.fn((url) => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days,
+      });
+    }
+
+    if (url === "api/appointments") {
+      // resolve apt data
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments,
+      });
+    }
+
+    if (url === "api/interviews") {
+      // resolve interviewers data
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers,
+      });
+    }
+  }),
+};
