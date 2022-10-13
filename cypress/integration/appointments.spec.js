bb1d7b15ac9e430e6759b77f0a8b33eb46a323f9
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 19:27:31 -0600

    add test for editting interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,20 +6,32 @@ describe("Appointments Tests", () => {
     cy.contains("Monday");
   });
 
-  it("should book an interview", () => {
+  xit("should book an interview", () => {
     // click on the + button with alt=Add
-    cy.get("[alt='Add']")
-      .first()
-      .click();
+    cy.get("[alt='Add']").first().click();
 
     // types input, picks interviewer and submits
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
 
-
     // verify that SHOW mode, displays name && interviewer
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt='Edit']").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
