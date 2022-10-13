98f05c626edaa5bb0f897522f09331490aa27d11
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 19:16:14 -0600

    refactor duplicate cypress before each test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,29 +1,25 @@
 describe("Appointments Tests", () => {
-  it("should book an interview", () => {
-    // 2. right here we send a request to the DB reset endpoint of our server
+  beforeEach(() => {
+    // Keeping it DRY, reset DB, visit site & wait for render each time
     cy.request("GET", "/api/debug/reset");
-
     cy.visit("/");
-    // make sure everything loads and can see monday
     cy.contains("Monday");
+  });
 
-    // click on the + button with alt=Add for the second appointment
+  it("should book an interview", () => {
+    // click on the + button with alt=Add
     cy.get("[alt='Add']")
-      .first() // because there are multiple Add buttons on the page
+      .first()
       .click();
 
-    // select input field, type name
+    // types input, picks interviewer and submits
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    // select photo with alt of interviewer name
     cy.get("[alt='Sylvia Palmer']").click();
-    // take a look at where an element contains "Save"
     cy.contains("Save").click();
 
-    // 1. Once we get here, the server state has changed and DB updates, we need to run a db reset request, that's why it's inserted up there
 
-    // Next we verify that SHOW works, that we see a student & interviewer
+    // verify that SHOW mode, displays name && interviewer
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    // saying, the elm that has this class, should contain... name && interviewer
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
