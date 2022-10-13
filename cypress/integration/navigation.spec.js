fa6132a13a05280629cdbc83e7ea4d1ab04086e5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 18:45:03 -0600

    finished the navigation tests, adding appointments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -7,10 +7,10 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     
-    // cy.contains("elem", "it's text") !== cy.get("elm").contains("text")
-    cy.contains("li", "Tuesday")
+    // updating this to use the data-testid="day"
+    cy.contains('[data-testid=day]', "Tuesday")
       .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)")
+      .should("have.class", "day-list__item--selected")
   })
   
 
