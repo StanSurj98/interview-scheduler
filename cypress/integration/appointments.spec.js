f50fa7a84c3ed6e8de92abc40f5c65093ba9e96e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 19:37:19 -0600

    add test for deleting an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -8,7 +8,9 @@ describe("Appointments Tests", () => {
 
   xit("should book an interview", () => {
     // click on the + button with alt=Add
-    cy.get("[alt='Add']").first().click();
+    cy.get("[alt='Add']")
+      .first()
+      .click();
 
     // types input, picks interviewer and submits
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
@@ -20,8 +22,11 @@ describe("Appointments Tests", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  it("should edit an interview", () => {
-    cy.get("[alt='Edit']").first().click({ force: true });
+  xit("should edit an interview", () => {
+    // The edit and delete buttons are both hidden unless hovered, use force click
+    cy.get("[alt='Edit']")
+      .first()
+      .click({ force: true });
 
     cy.get("[data-testid=student-name-input]")
       .clear()
@@ -33,5 +38,21 @@ describe("Appointments Tests", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt='Delete']")
+      .first()
+      .click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    // Let's make sure the status elm shows
+    cy.get("[alt='Loading']").should("exist");
+    cy.get("[alt='Loading']").should("not.exist");
+
+    // Lastly, check that the appointment card that shows Archie doesn't exist
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 });
