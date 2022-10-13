f13f01f2e8f2bd6d16d88000e54e5a848b055ca5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-12 21:42:03 -0600

    clean up comments, ready for submission

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,53 +1,45 @@
 export const getAppointmentsForDay = (state, dayArg) => {
-  // Refactored with help from Gary Jipp
   const results = [];
-  // 1. find the day obj
-  const day = state.days.find(d => d.name === dayArg)
+
+  // 1. find matching day
+  const day = state.days.find((d) => d.name === dayArg);
   if (!day) return [];
 
-  // 2. iterate thru that day's appointment ids (day.appoinments)
+  // 2. iterate through day's appointment id's
   for (const id of day.appointments) {
-    // 3. for each id, get the ACTUAL appointment object
-    const appointment = state.appointments[id]
-    // 4. add to our list (array of appointment objects)
+    // 3. for each id, get appointment object
+    const appointment = state.appointments[id];
+    // 4. add to our array of appointments
     results.push(appointment);
   }
   return results;
-
 };
 
 export const getInterview = (state, interview) => {
-  // Edge: if no interview object, return null
   if (!interview) return null;
-  // Interview is an object, {student: "string", interviewer: number (id)}
+
   // 1. look at interviewer id
   const id = interview.interviewer;
-  // 2. find the interviewer object at that id
-  const interviewerObj = state.interviewers[id]
+  // 2. find the interviewer with id
+  const interviewerObj = state.interviewers[id];
   // 3. return an object with student and interviewer
   return {
-    // whereas interview.student is a string primitive, so this is ref by VALUE already
     student: interview.student,
-    // Passing interviewer OBJECT by VALUE, that's why we use spread to make copy
-    interviewer: {...interviewerObj}
-  }
-
+    interviewer: { ...interviewerObj },
+  };
 };
 
 export const getInterviewersForDay = (state, dayArg) => {
-  // Find the correct day
-  const day = state.days.find(d => d.name === dayArg)
-  if (!day) return [];
-  
   const result = [];
 
-  // In the correct day, loop through the ids of interviewers array
-  // console.log(day.interviewers);
+  const day = state.days.find((d) => d.name === dayArg);
+  if (!day) return [];
+
+  // 1. Loop through interviewer id's for day
   for (const id of day.interviewers) {
-    // for each id, push into our array the interviewer object
+    // 2. Add to array of interviewers
     result.push(state.interviewers[id]);
   }
 
   return result;
-
 };
